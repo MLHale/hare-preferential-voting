3f1cab4de682c7cb0e378863e15475e9767eccab
Matt Hale <mlhale@unomaha.edu>
2018-04-24 02:30:55 -0500

    license

--- hare-pref-voting.py
+++ hare-pref-voting.py
@@ -3,8 +3,27 @@
 # @Email:  mlhale@unomaha.edu
 # @Filename: hare-pref-voting.py
 # @Last modified by:   matthale
-# @Last modified time: 2018-04-24T02:02:30-05:00
+# @Last modified time: 2018-04-24T02:30:39-05:00
 # @Copyright: Copyright (C) 2018 Matthew L. Hale
+
+"""
+Simple Hare preferential voting in python
+Copyright (C) 2018, Matt Hale
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import os
 import sys
 import getopt
